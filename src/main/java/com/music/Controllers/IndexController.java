4996e7f211de37f2673a29f770e0e31ff7d90384
ShreyasB02 <shreyasb2002@gmail.com>
2023-04-17 11:58:32 +0530

    Upload,Play,Pause,DB

--- src/main/java/com/music/Controllers/IndexController.java
+++ src/main/java/com/music/Controllers/IndexController.java
@@ -1,4 +1,34 @@
 package com.music.Controllers;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.music.services.StorageService;
+
+@Controller
 public class IndexController {
+    private final StorageService storageService;
+    @Autowired
+    public IndexController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+    
+    @GetMapping("/")
+    public String Home(Model model){
+        model.addAttribute("SongFileNames", storageService.getSongFileNames());
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String uploadSong(@RequestParam("file") MultipartFile file) throws IOException {
+        storageService.uploadSong(file);
+        return "redirect:/";
+    }
 }
